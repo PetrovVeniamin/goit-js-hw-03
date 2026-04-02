f0be381dd732b628cd282ec71a2e0697c2674f53
PetrovVeniamin <petrovviniamin495@gmail.com>
2026-04-02 12:06:05 +0200

    js-hw-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function makeArray(firstArray, secondArray, maxLength) {
   const newArray = firstArray.concat(secondArray);
-  if (newArray.Length > maxLength) {
-    return newArray.slice(0, maxLengthS);
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
   }
   return newArray;
 }
